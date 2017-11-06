a13d48cf577d45e5907383df25966fbc539b4d07
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-11-07 00:43:21 +0900

    Added deps for running gitlab

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:edge
 
-RUN apk add --no-cache vim sudo git ruby ruby-rdoc ruby-irb ruby-bundler ruby-dev ruby-rake go nodejs yarn postgresql-client
+RUN apk add --no-cache vim sudo git ruby ruby-bundler nodejs yarn postgresql-client
 
 ENV LANG=en_US.utf8
 
@@ -8,6 +8,7 @@ RUN sudo adduser -s /bin/nologin -g 'GitLab' -D git; \
 	chown -R git:git /home/git; \
 	cd /home/git; \
 	TAG=`git ls-remote -t https://gitlab.com/gitlab-org/gitlab-ce.git | grep -v -e '\^{}' -e 'rc[0-9]*' -e 'pre' | grep -o 'v10\..*$' | tail -1`; \
+	echo "Installing gitlab ${TAG}"; \
 	sudo -u git -H git clone --depth 1 -b ${TAG} https://gitlab.com/gitlab-org/gitlab-ce.git gitlab -v
 
 COPY assets /home/git/build/
--- assets/install.sh
+++ assets/install.sh
@@ -4,12 +4,16 @@ set -x
 
 cd /home/git/gitlab
 
-apk add --no-cache --virtual .builddev build-base icu-dev zlib-dev libffi-dev cmake krb5-dev postgresql-dev linux-headers re2-dev libassuan-dev libgpg-error-dev gpgme-dev
+apk add --no-cache --virtual .builddev build-base ruby-dev ruby-rake go icu-dev zlib-dev libffi-dev cmake krb5-dev postgresql-dev linux-headers re2-dev libassuan-dev libgpg-error-dev gpgme-dev
 
 sudo -u git -H echo "install: --no-document" > .gemrc
 
 sudo -u git -H bundle config --local build.gpgme --use-system-libraries
 
-sudo -u git -H bundle install --deployment --without development test mysql aws --verbose || echo "testing"
+sudo -u git -H bundle install --deployment --without development test mysql aws # --verbose || echo "testing"
 
-apk del .builddev
+apk del --no-cache .builddev
+
+RUNDEP=`scanelf --needed --nobanner --format '%n#p' --recursive /home/git/ | tr ',' '\n' | sort -u | awk 'system("[ -e /lib/" $1 " -o -e /usr/lib/" $1 " -o -e /usr/local/lib/" $1 " ]") == 0 { next } { print "so:" $1 }'`
+
+apk add --no-cache $RUNDEP
