1f960b3376ebd1a7e174a7b1f35cd3159365ab66
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-11-06 13:25:24 +0900

    Update install.sh for cleanup

--- assets/install.sh
+++ assets/install.sh
@@ -4,6 +4,8 @@ set -x
 
 cd /home/git/gitlab
 
-apk add --no-cache build-base icu-dev zlib-dev libffi-dev cmake krb5-dev postgresql-dev linux-headers re2-dev libassuan-dev libgpg-error-dev gpgme-dev
+apk add --no-cache --virtual .builddev build-base icu-dev zlib-dev libffi-dev cmake krb5-dev postgresql-dev linux-headers re2-dev libassuan-dev libgpg-error-dev gpgme-dev
 
 sudo -u git -H bundle install --deployment --without development test mysql aws --verbose || echo "testing"
+
+apk del .builddev
