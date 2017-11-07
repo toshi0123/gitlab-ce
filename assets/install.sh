f2decfe3e25315bbae7aef50b2edee9714b12235
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-11-07 10:25:09 +0900

    Added gitlab-shell etc.

--- assets/install.sh
+++ assets/install.sh
@@ -10,7 +10,25 @@ sudo -u git -H echo "install: --no-document" > .gemrc
 
 sudo -u git -H bundle config --local build.gpgme --use-system-libraries
 
-sudo -u git -H bundle install --deployment --without development test mysql aws # --verbose || echo "testing"
+# gitlab
+sudo -u git -H bundle install --deployment --without development test mysql aws -j2
+
+# gitlab-shell
+sudo -u git -H bundle exec rake gitlab:shell:install RAILS_ENV=production SKIP_STORAGE_VALIDATION=true
+
+# gitlab-workhorse
+sudo -u git -H bundle exec rake "gitlab:workhorse:install[/home/git/gitlab-workhorse]" RAILS_ENV=production
+
+# gitaly
+sudo -u git -H bundle exec rake "gitlab:gitaly:install[/home/git/gitaly]" RAILS_ENV=production
+
+# gettext
+sudo -u git -H bundle exec rake gettext:pack RAILS_ENV=production
+sudo -u git -H bundle exec rake gettext:po_to_json RAILS_ENV=production
+
+# assets
+sudo -u git -H yarn install --production --pure-lockfile
+sudo -u git -H bundle exec rake gitlab:assets:compile RAILS_ENV=production NODE_ENV=production
 
 apk del --no-cache .builddev
 
