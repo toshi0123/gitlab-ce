ddf3dbf3a4dfbe0a48b97695d58c9e6a8dd14e7c
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-11-09 15:27:55 +0900

    Modified for #6 building gitaly manually

--- assets/install.sh
+++ assets/install.sh
@@ -31,7 +31,20 @@ sudo -u git -H bundle exec rake gitlab:shell:install RAILS_ENV=production SKIP_S
 sudo -u git -H bundle exec rake "gitlab:workhorse:install[/home/git/gitlab-workhorse]" RAILS_ENV=production
 
 # gitaly
-sudo -u git -H bundle exec rake "gitlab:gitaly:install[/home/git/gitaly]" RAILS_ENV=production
+#sudo -u git -H bundle exec rake "gitlab:gitaly:install[/home/git/gitaly]" RAILS_ENV=production
+GITALY_SERVER_VERSION=$(cat /home/git/gitlab/GITALY_SERVER_VERSION)
+sudo -u git -H git clone --depth 1 -b v${GITALY_SERVER_VERSION} https://gitlab.com/gitlab-org/gitaly.git /home/git/gitaly
+
+cd /home/git/gitaly
+
+sudo -u git -H cp config.toml.example config.toml
+
+sed -i '/vendor\/bundle/d' Makefile
+sed -i 's/bundle install/bundle install --system/' Makefile
+
+make install
+
+cd -
 
 # gettext
 sudo -u git -H bundle exec rake gettext:pack RAILS_ENV=production > gettext_pack.log 2>&1
