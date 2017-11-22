0661deb780f98dc2d3b9b81a9ee7e908a93b2b9a
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-11-22 17:26:36 +0900

    Deleted define TAG in this shell

--- assets/download_gitlab.sh
+++ assets/download_gitlab.sh
@@ -2,9 +2,6 @@
 
 cd /home/git
 
-[ -z "$TAG" ] && \
-TAG=`git ls-remote -t https://gitlab.com/gitlab-org/gitlab-ce.git | grep -v -e '\^{}' -e 'rc[0-9]*' -e 'pre' | grep -o 'v10\..*$' | tail -1`
-
 echo "Downloading gitlab ${TAG}"
 
 sudo -u git -H git clone --depth 1 -b ${TAG} https://gitlab.com/gitlab-org/gitlab-ce.git gitlab -v
