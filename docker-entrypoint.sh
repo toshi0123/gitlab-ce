16847a72cfb5d998a38dd4da9b209dca727f2254
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-12-06 15:22:40 +0900

    Fixed db:setup to gitlab:setup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -102,7 +102,7 @@ cd /home/git/gitlab
 env PGPASSWORD="$DB_PASS" psql -h $DB_HOST -d $DB_NAME -U $DB_USER -c \
 "SELECT datname, pg_size_pretty(pg_database_size(datname)) FROM pg_database;" \
   | grep "$DB_NAME" | grep 'kB$' \
-&& { echo "yes" | sudo -u git -H bundle exec rake db:setup RAILS_ENV=production --trace || exit 1; } \
+&& { echo "yes" | sudo -u git -H bundle exec rake gitlab:setup RAILS_ENV=production --trace || exit 1; } \
 || { sudo -u git -H bundle exec rake db:migrate RAILS_ENV=production --trace || exit 1; }
 
 cp -pf /home/git/gitlab/VERSION /home/git/data/tmp/
