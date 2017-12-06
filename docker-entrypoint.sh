93e5197baaa177bb1246362b74bccea266e093a0
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-12-06 14:33:16 +0900

    Added initialize db

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,6 +20,19 @@ diff_config(){
   diff $1.example /home/git/data/config/example/$basename.example || exit 1
 }
 
+# set default
+DB_HOST=${DB_HOST:-gitlab-postgres}
+DB_PORT=${DB_PORT:-5432}
+DB_NAME=${DB_NAME:-gitlabhq_production}
+DB_USER=${DB_USER:-gitlab}
+DB_PASS=${DB_PASS:-gitlabpassword}
+
+env PGPASSWORD="$DB_PASS" psql -h $DB_HOST -d $DB_NAME -U $DB_USER -c \
+"SELECT true AS enabled FROM pg_available_extensions WHERE name = 'pg_trgm' AND installed_version IS NOT NULL;" || exit 1
+
+REDIS_HOST=${REDIS_HOST:-gitlab-redis}
+REDIS_PORT=${REDIS_PORT:-6379}
+
 if [ ! -d /home/git/data/config ];then
   mkdir -p /home/git/data/config
   mkdir -p /home/git/data/config/example
@@ -27,6 +40,24 @@ if [ ! -d /home/git/data/config ];then
   
   cp -pf /home/git/gitlab/config/database.yml.postgresql /home/git/gitlab/config/database.yml.example
   cp -pf /home/git/gitlab/lib/support/nginx/gitlab /etc/nginx/conf.d/gitlab.conf.example
+  
+  sed -i \
+  -e "s|database: .*$|database: $DB_NAME|g" \
+  -e "s|username: .*$|username: $DB_USER|g" \
+  -e "s|password: .*$|password: $DB_PASS|g" \
+  -e "s|host: .*$|host: $DB_HOST|g" \
+  -e "s|port: .*$|port: $DB_PORT|g" \
+    /home/git/gitlab/config/database.yml
+  
+  sed -i \
+  -e "s|unix:.*$|redis://$REDIS_HOST:$REDIS_PORT|g" \
+    /home/git/gitlab/config/resque.yml
+  
+  sed -i \
+  -e "s|host: .*$|host: $REDIS_HOST|g" \
+  -e "s|http://localhost/|http://localhost:8080/|g" \
+    /home/git/gitlab-shell/config.yml
+  
   while read line;do
     prepare_config $line
   done < configfile_list.txt
@@ -60,12 +91,16 @@ rm -rf /home/git/gitlab/log
 ln -s /var/log/gitlab /home/git/gitlab/log
 chown git:git /var/log/gitlab
 
-if [ -e /home/git/data/config/VERSION ];then
-  cd /home/git/gitlab
-  sudo -u git -H bundle exec rake db:migrate RAILS_ENV=production --trace || exit 1
-fi
+[ -e /home/git/data/tmp/VERSION ] && diff /home/git/data/tmp/VERSION /home/git/gitlab/VERSION
 
-cp -pf /home/git/gitlab/VERSION /home/git/data/config/
+cd /home/git/gitlab
+env PGPASSWORD="$DB_PASS" psql -h $DB_HOST -d $DB_NAME -U $DB_USER -c \
+"SELECT datname, pg_size_pretty(pg_database_size(datname)) FROM pg_database;" \
+  | grep "$DB_NAME" | grep 'kB$' \
+&& { echo "yes" | sudo -u git -H bundle exec rake db:setup RAILS_ENV=production --trace || exit 1; } \
+|| { sudo -u git -H bundle exec rake db:migrate RAILS_ENV=production --trace || exit 1; }
+
+cp -pf /home/git/gitlab/VERSION /home/git/data/tmp/
 
 /etc/init.d/gitlab start || exit 1
 /usr/sbin/nginx
