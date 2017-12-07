23416c6bf5326f216c843e38768ecfc3b15095a1
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-12-07 19:36:51 +0900

    Modified default of nginx.config

--- assets/default_setting.sh
+++ assets/default_setting.sh
@@ -26,6 +26,7 @@ ln -s /bin/pkill /usr/bin/pkill
 
 mkdir -p /run/nginx
 rm -f /etc/nginx/conf.d/default.conf
+sed -i 's/ssl_session_cache/#ssl_session_cache/' /etc/nginx/nginx.conf
 
 # default settings of data volume
 sed -i 's|/home/git/.ssh/authorized_keys|/home/git/data/.ssh/authorized_keys|g' /home/git/gitlab-shell/config.yml
