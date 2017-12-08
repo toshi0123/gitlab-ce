5c929d452631750ad131ea29f6dddb72806ff2cd
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-12-08 15:22:41 +0900

    Added same as v10.0.7

--- assets/install_assets.sh
+++ assets/install_assets.sh
@@ -8,4 +8,5 @@ sudo -u git -H bundle exec rake gettext:po_to_json RAILS_ENV=production
 
 # assets
 sudo -u git -H yarn install --production --pure-lockfile
+sudo -u git -H yarn add ajv@^4.0.0
 sudo -u git -H bundle exec rake gitlab:assets:compile RAILS_ENV=production NODE_ENV=production > assets.log 2>&1 || cat assets.log
