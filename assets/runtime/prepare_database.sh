523f21ae08e0570545a6be1bb17778d0d4cbd780
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-12-22 15:14:05 +0900

    Update prepare_database.sh

--- assets/runtime/prepare_database.sh
+++ assets/runtime/prepare_database.sh
@@ -3,7 +3,6 @@
 cd /home/git/gitlab
 
 env PGPASSWORD="$DB_PASS" psql -h $DB_HOST -d $DB_NAME -U $DB_USER -c \
-"SELECT datname, pg_size_pretty(pg_database_size(datname)) FROM pg_database;" \
-  | grep "$DB_NAME" | grep 'kB$' \
+"\dt" | grep '^Did not find any relations.' \
 && { echo "yes" | sudo -u git -H bundle exec rake gitlab:setup RAILS_ENV=production --trace || exit 1; } \
 || { sudo -u git -H bundle exec rake db:migrate RAILS_ENV=production --trace || exit 1; }
