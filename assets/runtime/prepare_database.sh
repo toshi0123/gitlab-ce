2dd1e8f575a4f9148d0adf354d6fd8ea8e0df49f
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2017-12-22 15:41:55 +0900

    Update prepare_database.sh

--- assets/runtime/prepare_database.sh
+++ assets/runtime/prepare_database.sh
@@ -3,6 +3,6 @@
 cd /home/git/gitlab
 
 env PGPASSWORD="$DB_PASS" psql -h $DB_HOST -d $DB_NAME -U $DB_USER -c \
-"\dt" | grep '^Did not find any relations.' \
+"\dt" 2>&1 | grep '^Did not find any relations.' \
 && { echo "yes" | sudo -u git -H bundle exec rake gitlab:setup RAILS_ENV=production --trace || exit 1; } \
 || { sudo -u git -H bundle exec rake db:migrate RAILS_ENV=production --trace || exit 1; }
