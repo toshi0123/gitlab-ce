a32d436058db3b7f99b0711ba3229769dc8e16b3
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2018-01-15 11:31:52 +0900

    add ajv

--- assets/build/install_assets.sh
+++ assets/build/install_assets.sh
@@ -8,4 +8,5 @@ sudo -u git -H bundle exec rake gettext:po_to_json RAILS_ENV=production --trace
 
 # assets
 sudo -u git -H yarn install --production --pure-lockfile
+sudo -u git -H yarn add ajv@^4.0.0
 sudo -u git -H bundle exec rake gitlab:assets:compile RAILS_ENV=production NODE_ENV=production --trace > assets.log 2>&1 || { cat assets.log;exit 1; }
