71366968893c361995e9df608c0e67459f1028a9
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2018-01-15 17:20:40 +0900

    update grpc

--- assets/build/install_gitlab.sh
+++ assets/build/install_gitlab.sh
@@ -11,6 +11,6 @@ sudo -u git -H bundle config --local build.gpgme --use-system-libraries
 sudo -u git -H mkdir -p /home/git/repositories
 
 sed -i 's/google-protobuf (3.2.0.2)/google-protobuf (3.3.0)/g' Gemfile.lock
-sed -i 's/grpc (1.2.5)/grpc (1.4.0)/g' Gemfile.lock
+sed -i 's/grpc (.*)/grpc (1.4.0)/g' Gemfile.lock
 
 sudo -u git -H bundle install --system --without development test mysql aws kerberos -j$(nproc)
