c1b53e96d1d8db56e038f3d9f316f2c4a01e2cdc
toshi0123 <7948737+toshi0123@users.noreply.github.com>
2019-03-16 22:37:34 +0900

    Update prepare_database.sh

--- assets/runtime/prepare_database.sh
+++ assets/runtime/prepare_database.sh
@@ -5,7 +5,7 @@ cd /home/git/gitlab
 env PGPASSWORD="$DB_PASS" psql -h $DB_HOST -d $DB_NAME -U $DB_USER -c \
 "\dt" 2>&1 | grep '^Did not find any relations.' \
 && { 
- sudo -u git -H gitaly /etc/gitlab/config.toml &
+ sudo -u git -H gitaly /etc/gitlab/config.toml >> /var/log/gitlab/gitaly.log 2>&1 &
  echo "yes" | sudo -u git -H bundle exec rake gitlab:setup RAILS_ENV=production --trace || exit 1
  pkill gitaly
 } || { 
